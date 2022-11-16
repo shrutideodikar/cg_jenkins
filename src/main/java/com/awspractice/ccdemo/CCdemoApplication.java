b7afd840e37ab9e129568373446c2fc02ab3bea3
shrutideodikar <shrutideodikar@gmail.com>
2022-11-16 12:46:08 +0530

    first commit

---
+++ src/main/java/com/awspractice/ccdemo/CCdemoApplication.java
@@ -0,0 +1,13 @@
+package com.awspractice.ccdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CCdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awspractice/ccdemo/CCdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awspractice.ccdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CCdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
